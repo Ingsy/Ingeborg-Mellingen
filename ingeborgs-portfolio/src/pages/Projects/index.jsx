dbcdb56074f870631a5507b19902702a12196057
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-10-11 14:21:42 +0200

    Adding projects

--- ingeborgs-portfolio/src/pages/Projects/index.jsx
+++ ingeborgs-portfolio/src/pages/Projects/index.jsx
@@ -3,28 +3,31 @@ import styles from "./projects.module.css"; // Import a CSS file for styling
 
 const Project = ({ logo, name, description, githubLink, deployedLink }) => {
   return (
-    <div className={styles.projects}>
-      <img src={logo} alt={name} />
-      <h3 className={styles.projectName}>{name}</h3>
-      <p className={styles.projectDescription}>{description}</p>
-      <a
-        className={styles.projectLink}
-        href={githubLink}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-      <a
-        className={styles.projectLink}
-        href={deployedLink}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Deployed
-      </a>
+    <div className={styles.project}>
+      <img className={styles.projectImg} src={logo} alt={name} />
+      <div className={styles.projectInfo}>
+        <div className={styles.middleSection}>
+          <h3 className={styles.projectName}>{name}</h3>
+          <a
+            className={styles.projectLink}
+            href={githubLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          <a
+            className={styles.projectLink}
+            href={deployedLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Deployed
+          </a>
+        </div>
+        <p className={styles.projectDescription}>{description}</p>
+      </div>
     </div>
   );
 };
-
 export default Project;
--- ingeborgs-portfolio/src/pages/Projects/projectPage.jsx
+++ ingeborgs-portfolio/src/pages/Projects/projectPage.jsx
@@ -6,21 +6,23 @@ const ProjectsPage = () => {
     {
       name: "BeFriendMe",
       logo: "project1_logo.png",
-      description: "Description for Project 1",
+      description:
+        "BeFriendMe is a social media application. The API routes requires authorisation via JWT (JSON Web Tokens). localStorage is used for storing JWT  HTTP methods: POST, GET, PUT, and DELETE to enable CRUD operations.",
       githubLink: "https://github.com/project1",
       deployedLink: "https://project1.com",
     },
     {
       name: "Auction Site",
-      logo: "project2_logo.png",
-      description: "Description for Project 2",
-      githubLink: "https://github.com/project2",
-      deployedLink: "https://project2.com",
+      logo: "BID.png",
+      description:
+        "BID is an auction website. A registered user may create, view, search and add a bid to other users listings. HTTP methods: POST, GET, PUT, and DELETE to enable CRUD operations. Static host: Build and deployed to github pages",
+      githubLink: "https://github.com/Ingsy/ingsy.github.io",
+      deployedLink: "ingsy.github.io/",
     },
     {
       name: "Online Shop",
-      logo: "project2_logo.png",
-      description: "Description for Project 2",
+      logo: "Shop.png",
+      description: "Online Shop is an eCom store build with React",
       githubLink: "https://github.com/project2",
       deployedLink: "https://project2.com",
     },
